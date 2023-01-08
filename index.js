b3a71370a0caadd0ae42c05b7afa9c4808d742c0
saleempirwani <salimraza3465@gmail.com>
2023-01-08 13:25:24 +0500

    init setup added

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  res.send({ message: "Hello World" });
+});
+
+app.listen(port, () => {
+  console.log(`Server is listening on port http://localhost:${port}`);
+});
